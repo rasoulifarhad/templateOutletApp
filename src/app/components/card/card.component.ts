b6335eccb559ea682c40efe9cae187366d25f8cb
Farhad rasouli <rasouli.farhad@gmail.com>
2023-11-17 16:18:28 +0330

    updated

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+
+  // title could be a string or a TemplateRef. In case it is a string we will use string interpolation to bind
+  // it to the template, otherwise, we will use ngTemplateOutlet.
   @Input()
-  title! : string;
+  title?: string;
+  @Input()
+  titleTemplate: TemplateRef<any> | null = null;
+
+
+  isTitleAsString = () => typeof this.title == 'string';
 }
